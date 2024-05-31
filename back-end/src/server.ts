0044c79366df024f7a1a8a4c560574cf8492e9dc
Murillou <murillovinicius13@hotmail.com>
2024-05-31 18:52:13 -0300

    [feat] instalando depêndencias na aplicação

---
+++ back-end/src/server.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify';
+import { usersRoutes } from './routes/users';
+
+export const app = fastify();
+
+app.register(usersRoutes, {
+  prefix: 'users',
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP Server Running');
+});
