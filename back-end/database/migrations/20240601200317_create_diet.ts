8139cf2736ef5e28a493264998c45964f38f4033
Murillou <murillovinicius13@hotmail.com>
2024-06-01 17:19:24 -0300

    [feat] tabela de dietas criada

--- back-end/database/migrations/20240601200317_create_diet.ts
+++ back-end/database/migrations/20240601200317_create_diet.ts
@@ -1,10 +1,15 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('diets', table => {
+    table.uuid('id').primary();
+    table.text('name').notNullable();
+    table.text('description').notNullable();
+    table.timestamp('time_diet').defaultTo(knex.fn.now()).notNullable();
+    table.text('diet_or_no').notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('diets');
 }
-
