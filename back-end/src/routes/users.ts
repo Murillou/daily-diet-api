6047c4689b2fec9462aafcd8126eceaf7888b4d0
Murillou <murillovinicius13@hotmail.com>
2024-06-01 16:51:44 -0300

    [feat] adicionado o cookie para identificar usuário

--- back-end/src/routes/users.ts
+++ back-end/src/routes/users.ts
@@ -5,28 +5,38 @@ import crypto from 'node:crypto';
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', { preHandler: checkSessionIdExists }, async request => {
-    const users = await knex('users').select('*');
+  app.get('/', { preHandler: [checkSessionIdExists] }, async request => {
+    const sessionId = request.cookies;
+
+    const users = await knex('users').where('session_id', sessionId).select();
 
     return { users };
   });
 
-  app.post(
-    '/',
-    { preHandler: checkSessionIdExists },
-    async (request, reply) => {
-      const createUsersBodySchema = z.object({
-        name: z.string(),
+  app.post('/', async (request, reply) => {
+    const createUsersBodySchema = z.object({
+      name: z.string(),
+    });
+
+    const { name } = createUsersBodySchema.parse(request.body);
+
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID();
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
       });
-
-      const { name } = createUsersBodySchema.parse(request.body);
-
-      await knex('users').insert({
-        id: crypto.randomUUID(),
-        name,
-      });
-
-      return reply.status(201).send();
     }
-  );
+
+    await knex('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      session_id: sessionId,
+    });
+
+    return reply.status(201).send();
+  });
 }
